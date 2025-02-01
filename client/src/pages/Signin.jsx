637b13bf4dac5933e0266ca9cbf9756f9772ff72
shivang verma <shivangverma43@gmail.com>
2025-02-01 19:33:26 +0530

    cors err fixed

--- client/src/pages/Signin.jsx
+++ client/src/pages/Signin.jsx
@@ -23,7 +23,7 @@ export default function Signin() {
       });
 
       // Save the token to 
-      console.log(resp);
+      // console.log(resp);
       // console.log(resp.data.token)
       localStorage.setItem("token",resp.data.token);
       // Navigate to the dashboard
